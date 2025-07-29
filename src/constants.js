8acf3369b3ff7c361fff69cca7f5db1bc99be357
alejandro huanca <alejandroesta55@gmail.com>
2025-07-28 20:24:32 -0400

    Act3.3 fix menores

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ export const HERO_VIDEO_URL = 'https://videos.pexels.com/video-files/853874/8538
 export const ADMISSIONS_VIDEO_URL = 'https://videos.pexels.com/video-files/3254009/3254009-hd_1920_1080_25fps.mp4';
 
 // --- Enlaces a Redes Sociales y Contacto ---
-export const LINKEDIN_URL = 'https://linkedin.com/school/TU_PERFIL_LINKEDIN';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/colegio-am%C3%A9rica-de-atlantis-10b720377/';
 export const INSTAGRAM_URL = 'https://www.instagram.com/colegio_america_de_atlantis?utm_source=qr&igsh=N2VvbWR6NzdwcHc0';
 export const FACEBOOK_URL = 'https://www.facebook.com/people/Escuela-Am%C3%A9rica-Atlantis/61578847313878/?rdid=8VwZQRK6y4bnzude&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F16jgJ1n8YK%2F';
 export const WHATSAPP_URL = 'https://chat.whatsapp.com/EOJz26f2rSl2x0Mp4udsJi';
